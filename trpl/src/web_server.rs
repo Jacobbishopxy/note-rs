4c5c76f85aa83edd2551b372e3c01f41fb172cf0
Jacob <jacobbishopxy@gmail.com>
2021-04-18 13:59:33 +0800

    update Chapter 20's example

--- trpl/src/web_server.rs
+++ trpl/src/web_server.rs
@@ -3,26 +3,63 @@ use std::thread;
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.", id);
-            job();
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
 }
 
 impl ThreadPool {
@@ -55,6 +92,6 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
--- trpl_example/src/bin/web_server.rs
+++ trpl_example/src/bin/web_server.rs
@@ -18,7 +18,25 @@ fn main() {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.")
 }
+
+/// testing case
+// fn main() {
+//     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+//     let pool = ThreadPool::new(4);
+
+//     for stream in listener.incoming().take(2) {
+//         let stream = stream.unwrap();
+
+//         pool.execute(|| {
+//             handle_connection(stream);
+//         });
+//     }
+
+//     println!("Shutting down.");
+// }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
