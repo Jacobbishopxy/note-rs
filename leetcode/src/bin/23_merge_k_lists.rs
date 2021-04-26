efc9d6115c60d343e55a846d06b9c69664e00aca
Jacob <jacobbishopxy@gmail.com>
2021-04-26 23:00:11 +0800

    23_merge_k_lists

---
+++ leetcode/src/bin/23_merge_k_lists.rs
@@ -0,0 +1,102 @@
+fn main() {
+    let a1 = ListNode::create(vec![1, 4, 5]);
+    let a2 = ListNode::create(vec![1, 3, 4]);
+    let a3 = ListNode::create(vec![2, 6]);
+
+    let foo = vec![a1, a2, a3];
+
+    println!("result: {:?}", merge_k_lists(foo));
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+
+    pub fn create(vs: Vec<i32>) -> Option<Box<ListNode>> {
+        if vs.len() == 0 {
+            return None;
+        }
+
+        let mut res: Option<Box<ListNode>> = None;
+        let mut next = &mut res;
+
+        for &v in vs.iter() {
+            *next = Some(Box::new(ListNode::new(v)));
+
+            if let Some(b) = next {
+                next = &mut b.next;
+            }
+        }
+
+        res
+    }
+
+    pub fn to_vec(&self) -> Vec<i32> {
+        let mut res = vec![self.val];
+        let mut next = &self.next;
+
+        loop {
+            match next {
+                Some(v) => {
+                    res.push(v.val);
+                    next = &v.next;
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+
+        res
+    }
+}
+
+pub fn merge_k_lists(mut lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+    if lists.is_empty() {
+        return None;
+    }
+
+    let mut len = lists.len();
+
+    while len > 1 {
+        for i in 0..len / 2 {
+            // 头尾项合并，形成新的 lists。lists 的长度为奇数时，最中间的项保留变为新 lists 的最后一项。
+            lists[i] = M::merge_two_lists(lists[i].take(), lists[len - i - 1].take());
+        }
+        // list 的长度为奇数时，+1 使其成为偶数再被 2 除，作为下一次 while loop 的执行条件
+        len = (len + 1) / 2;
+    }
+
+    lists[0].take()
+}
+
+pub struct M;
+
+impl M {
+    pub fn merge_two_lists(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        match (l1, l2) {
+            (None, None) => None,
+            (None, r) => r,
+            (l, None) => l,
+            (Some(mut l), Some(mut r)) => {
+                if l.val <= r.val {
+                    l.next = Self::merge_two_lists(l.next, Some(r));
+                    Some(l)
+                } else {
+                    r.next = Self::merge_two_lists(Some(l), r.next);
+                    Some(r)
+                }
+            }
+        }
+    }
+}
