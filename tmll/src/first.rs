dea52a2f6e8e973f8c2468cc7f69c99292506587
Jacob Xie <jacobbishopxy@gmail.com>
2021-04-28 10:29:21 +0800

    TMLL 2.4 & 2.5

--- tmll/src/first.rs
+++ tmll/src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -10,4 +12,25 @@ enum Link {
 struct Node {
     elem: i32,
     next: Link,
+}
+
+impl List {
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
 }
