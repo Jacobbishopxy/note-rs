f4e49d91e97605d17951ad02dec7666b07c7883a
Jacob <jacobbishopxy@gmail.com>
2021-05-02 09:33:15 +0800

    TMLL 5.1

---
+++ tmll/src/fourth.rs
@@ -0,0 +1,18 @@
+//! fourth.rs
+//! TMLL chapter 4
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
--- tmll/src/lib.rs
+++ tmll/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod first;
+pub mod fourth;
 pub mod second;
 pub mod third;
