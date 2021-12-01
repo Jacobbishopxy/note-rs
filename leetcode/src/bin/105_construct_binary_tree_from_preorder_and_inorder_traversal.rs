e5725bccf0ce442df09c11e1c5d770823357e6e1
Jacob Xie <jacobbishopxy@gmail.com>
2021-12-01 10:46:44 +0800

    105_build_tree

---
+++ leetcode/src/bin/105_construct_binary_tree_from_preorder_and_inorder_traversal.rs
@@ -0,0 +1,44 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use leetcode::util::TreeNode;
+
+fn main() {
+    let po = vec![1, 2, 4, 7, 3, 5, 6, 8];
+    let io = vec![4, 7, 2, 1, 5, 3, 8, 6];
+
+    println!("{:?}", build_tree(po, io));
+}
+
+fn build_tree(mut preorder: Vec<i32>, mut inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+    let len = inorder.len() as i32 - 1;
+    return build(&mut preorder, 0, len, &mut inorder, 0, len);
+}
+
+fn build(
+    preorder: &mut Vec<i32>,
+    pl: i32,
+    pr: i32,
+    inorder: &mut Vec<i32>,
+    il: i32,
+    ir: i32,
+) -> Option<Rc<RefCell<TreeNode>>> {
+    if pl > pr || il > ir {
+        return None;
+    }
+    let mut root = TreeNode {
+        val: preorder[pl as usize],
+        left: None,
+        right: None,
+    };
+
+    let mut rootin = il;
+    while rootin <= ir && inorder[rootin as usize] != root.val {
+        rootin += 1;
+    }
+    let l = rootin - il;
+    root.left = build(preorder, pl + 1, pl + l, inorder, il, rootin - 1);
+    root.right = build(preorder, pl + l + 1, pr, inorder, rootin + 1, ir);
+
+    return Some(Rc::new(RefCell::new(root)));
+}
